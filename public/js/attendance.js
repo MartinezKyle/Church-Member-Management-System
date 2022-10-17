da20bee63f207a346a2334486826c1814a95d49e
PagMaz <104805903+PagMaz@users.noreply.github.com>
2022-10-17 22:56:09 +0800

    Create attendance.js

---
+++ public/js/attendance.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	$("#submit").click(function () {
+        var phonenum = document.querySelector("#phonenum");
+		var session = document.querySelector("#session");
+		
+        if (phonenum.value != "" && session.value != "") {
+			var url = `/addAttendance?phonenum=${phonenum.value}&session=${session.value}`;
+			
+			$.get(url, (data, status, xhr) => {
+                alert(status);
+                if (status == "success") {
+                    console.log("HELLO");
+                }
+            });
+        }
+		else {
+			document.querySelector("#errorText").innerHTML = "";
+			document.querySelector("#errorText").innerHTML += "Fill up all fields.";
+			console.log("error");
+		}
+    });   
+});
