29d7e67bb2b375de533f0ff745e98eb5a2d64994
Kyle-Martinez <115062098+MartinezKyleX@users.noreply.github.com>
2022-10-20 19:44:23 +0800

    Fixed log-in authentication

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var errorText = document.querySelector('#errorText');
-    var form = document.querySelector('#login');
+    //var errorText = document.getElementById("errorText");
 	$("#submit").click(function () {
         var number = document.querySelector('#phonenum');
         var pass = document.querySelector('#password');
-        var url = `/CheckLogin?phonenum=${number.value}&password=${pass.value}&permission=admin`;
-		
+        		
         if(number.value != "" && pass.value != ""){
+            var url = `/CheckLogin?phonenum=${number.value}&password=${pass.value}&permission=admin`;
             $.post(url, (data, status, xhr) => {
                 if(status == "success"){
                     if(!data){
-                        errorText.innerHTML = "Account with this phonenum/password does not exist";
+                        document.getElementById("errorText").innerHTML = "Admin account with this phonenum/password does not exist";
                     } else {
                         alert("Logged in");
                         window.location.href = '/loadMembers';
                     }
                 }
             });
+            var form = document.getElementById("login");
             form.reset();
         } else {
-			document.querySelector("#errorText").innerHTML = "";
-			document.querySelector("#errorText").innerHTML += "Enter registered number/password.";
+			document.getElementById("errorText").innerHTML = "";
+			document.getElementById("errorText").innerHTML += "Enter registered number/password.";
 			console.log("error");
         }
     });
