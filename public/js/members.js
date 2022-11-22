09e37b043feea06b6030addf9c2f15fc15ed4283
Raphael Roncal <raph.roncal01@gmail.com>
2022-11-22 11:51:03 +0800

    added comment

--- public/js/members.js
+++ public/js/members.js
@@ -18,3 +18,20 @@ $(document).ready(() => {
         });
     });
 })
+
+// I don't think na na-implement ko 'to correctly. Please confirm
+// 
+// 
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     $(".delete").click(function () {
+//         var phonenum = $(this).val();
+//         console.log(phonenum);
+//         var url = '/deleteMember?phonenum=' + phonenum;
+//         $.get(url, (data, status, xhr) => {
+//             alert(status);
+//             if (status == "success") {
+//                 window.location.href = "/loadMembers";
+//             }
+//         });
+//     });
+// });
