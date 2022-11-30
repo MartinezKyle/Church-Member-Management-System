ef64241df4db5f96802cc1edbc38b6ee344017e3
DrNARP <119021632+DrNARP@users.noreply.github.com>
2022-11-30 17:32:50 +0800

    Session Handling

--- public/js/login.js
+++ public/js/login.js
@@ -6,13 +6,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
         		
         if(number.value != "" && pass.value != ""){
             var url = `/CheckLogin?phonenum=${number.value}&password=${pass.value}`;
+            var auth = `/AllowLogin?phonenum=${number.value}`;
+            var sesh = `/createSession?phonenum=${number.value}`;
+            
             $.post(url, (data, status, xhr) => {
                 if(status == "success"){
                     if(!data){
                         document.getElementById("errorText").innerHTML = "Admin account with this phonenum/password does not exist";
                     } else {
-                        // alert("Logged in");
-                        window.location.href = '/loadMembers';
+                        //alert("Logged in");
+                        $.get(sesh, (data, status, xhr) => {
+                            if(status == 'success'){
+                                console.log("Session Created");
+                            } else {
+                                console.log("An Error Occurred");
+                            }
+                        });
+                        $.get(auth, (data, status, xhr) => {
+                            if(status == 'success'){
+                                window.location.href = '/loadMembers';
+                            } else {
+                                console.log("An Error Occurred");
+                            }
+                        });
                     }
                 }
             });
