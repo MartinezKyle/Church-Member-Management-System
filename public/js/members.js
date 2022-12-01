32c52b26fc59fb489e2cfd686b0908b47131d9b5
Kyle-Martinez <115062098+MartinezKyleX@users.noreply.github.com>
2022-12-01 22:04:34 +0800

    Reverted delete changes

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,19 @@
-$(document).ready(() => {
+document.addEventListener("DOMContentLoaded", function (event) {
+    $(".delete").click(function () {
+        var phonenum = $(this).val();
+        console.log("hello");
+        console.log(phonenum);
+        var url = '/deleteMember?phonenum=' + phonenum;
+        $.get(url, (data, status, xhr) => {
+            alert(status);
+            if (status == "success") {
+                window.location.href = "/loadMembers";
+            }
+        });
+    });
+});
+
+/*$(document).ready(() => {
     let phonenum;
 
     $("#trigger-delete-modal").click(function () {
@@ -17,21 +32,5 @@ $(document).ready(() => {
             }
         });
     });
-})
+})*/
 
-// I don't think na na-implement ko 'to correctly. Please confirm
-// 
-// 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     $(".delete").click(function () {
-//         var phonenum = $(this).val();
-//         console.log(phonenum);
-//         var url = '/deleteMember?phonenum=' + phonenum;
-//         $.get(url, (data, status, xhr) => {
-//             alert(status);
-//             if (status == "success") {
-//                 window.location.href = "/loadMembers";
-//             }
-//         });
-//     });
-// });
